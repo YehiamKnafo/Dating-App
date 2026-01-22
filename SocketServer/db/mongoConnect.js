23b3b797a28205ba6f4cf06bc0da1c8abda76221
yehiamtheone <yehiamknafo5757@gmail.com>
2026-01-22 22:07:32 +0200

    updated mongoconnect.js

--- SocketServer/db/mongoConnect.js
+++ SocketServer/db/mongoConnect.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectToDB = async() =>  {
     // mongoose.set('strictQuery' , false);
 
-    await mongoose.connect("mongodb+srv://yehiam:159654qa@cluster0.eyhd9.mongodb.net/forrealdatingapp");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("mongo connect started");
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
