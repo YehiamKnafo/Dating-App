11521feff04e45a6f6df0c26b617d21c368b38f4
yehiamtheone <yehiamknafo5757@gmail.com>
2026-04-05 23:24:58 +0300

    Updated second backend route

--- Frontend/src/main/java/forrealdatingapp/routes/CredentialsRequests.java
+++ Frontend/src/main/java/forrealdatingapp/routes/CredentialsRequests.java
@@ -24,7 +24,7 @@ public class CredentialsRequests {
 
         Request request = new Request.Builder()
                 //SECRET BACKEND NOT THE MAIN ONE
-                .url("https://datingappcredentials-latest.onrender.com/")
+                .url("https://yehiam-offical-dating-app-website.onrender.com")
                 .addHeader("X-App-Signature", "JavaFX-Client-v1")
                 .build();
 
